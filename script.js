515edf1459f8a8f225a9064f6029c55a6a690e15
Rezuwaann <ahmedrezuwan20@gmail.com>
2025-08-26 12:50:43 +0600

    login page done

---
+++ script.js
@@ -0,0 +1,21 @@
+// login button functionality
+document.getElementById('login-btn').addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const mobileNum = 1234;
+    const pinNum = 1234;
+
+    const userNum = document.getElementById('number-input').value;
+    const userPin = document.getElementById('pin-input').value;
+
+    const userNumConverted = parseInt(userNum);
+    const userPinConverted = parseInt(userPin);
+
+    if (mobileNum === userNumConverted && pinNum === userPinConverted) {
+        window.location.href = './home.html';
+    } else if (mobileNum !== userNumConverted) {
+        alert('Wrong Number')
+    }else {
+        alert('Wrong Pin')
+    }
+})
